020c6ca1237e35e9c1d073c78da9476e36ba8f28
Alexander <psashv@gmail.com>
2023-04-13 11:11:10 +0300

    add ui

--- game.js
+++ game.js
@@ -21,33 +21,38 @@ function playRound(playerSelection, computerSelection) {
   return 'pc'
 }
 
-function game(rounds = 5) {
-  let playerScore = 0;
-  let pcScore = 0;
-  for (let i = 0; i < rounds; i++) {
-    let playerChoice = prompt('Rock, Paper or Scissors: ', 'Rock').toLowerCase();
-    let pcChoice = getComputerChoice();
+let winScore = 5;
+let playerScore = 0;
+let pcScore = 0;
+let round = 0;
 
-    let roundResult = playRound(playerChoice, pcChoice);
-    
-    playerChoice = toCapital(playerChoice);
-    pcChoice = toCapital(pcChoice);
+function play() {
+  let playerChoice = this.dataset.key;
+  let pcChoice = getComputerChoice();
 
-    let message = `Round ${i + 1}: `;
-    if (roundResult === 'draw') {
-      message += `is a draw. Both choose ${playerChoice}`;
-    } else if (roundResult === 'player') {
-      message += `You Won! ${playerChoice} beats ${pcChoice}`;
-      playerScore++;
-    } else {
-      message += `You Lose! ${pcChoice} beats ${playerChoice}`;
-      pcScore++;
-    }
-    console.log(message);
+  let roundResult = playRound(playerChoice, pcChoice);
+
+  playerChoice = toCapital(playerChoice);
+  pcChoice = toCapital(pcChoice);
+
+  let message = `Round ${++round}: `;
+  if (roundResult === 'draw') {
+    message += `is a draw. Both choose ${playerChoice}`;
+  } else if (roundResult === 'player') {
+    message += `You Won! ${playerChoice} beats ${pcChoice}`;
+    playerScore++;
+  } else {
+    message += `You Lose! ${pcChoice} beats ${playerChoice}`;
+    pcScore++;
   }
-  let gameResult =
-  playerScore === pcScore ? "There's no winner, result is a draw!" :
-  playerScore > pcScore ? "Winner - player" : "Winner - pc";
-  return gameResult
+  console.log(message);
 }
-console.log(game());
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', play));
+
+// let gameResult =
+//   playerScore === pcScore ? "There's no winner, result is a draw!" :
+//     playerScore > pcScore ? "Winner - player" : "Winner - pc";
+// return gameResult
+// console.log('result');
