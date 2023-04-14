5f15f8249ed036f5e4dedfe9117337a8ed9796c6
Alexander <psashv@gmail.com>
2023-04-14 13:24:52 +0300

    make ui interactive

--- game.js
+++ game.js
@@ -1,58 +1,52 @@
 'use strict';
 
-const playOptions = ['rock', 'paper', 'scissors'];
-
-let getRandomInt = n => Math.floor(Math.random() * n);
-// -> 0 ... n - 1
-
-let getComputerChoice = () => playOptions[getRandomInt(playOptions.length)];
-
-let toCapital = (word) => word[0].toUpperCase() + word.slice(1);
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection)
-    return `draw`
-  if (playerSelection === 'rock' && computerSelection === 'scissors')
-    return 'player'
-  if (playerSelection === 'paper' && computerSelection === 'rock')
-    return 'player'
-  if (playerSelection === 'scissors' && computerSelection === 'paper')
-    return 'player'
-  return 'pc'
-}
-
-let winScore = 5;
-let playerScore = 0;
-let pcScore = 0;
+const goal = 5;
 let round = 0;
 
-function play() {
+const playOptions = ['Rock', 'Paper', 'Scissors'];
+const getRandomInt = n => Math.floor(Math.random() * n);
+// -> 0 ... n - 1
+const getComputerChoice = () => playOptions[getRandomInt(playOptions.length)];
+
+function updateScore(winner) {
+  let score = ++document.querySelector(`#${winner}`).value;
+  if (score == goal) {
+    buttons.forEach(button => button.removeEventListener('click', playRound));
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.winner').textContent =
+      `Winner - ${winner.toUpperCase()}!`;
+  }
+}
+
+const result = document.querySelector('.result');
+function appendRoundResult(text) {
+  let para = document.createElement('p');
+  para.textContent = text;
+  result.appendChild(para);
+}
+
+function getResult(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection)
+    return `It's a draw. Both choose ${playerSelection}`
+
+  if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+    playerSelection === 'Paper' && computerSelection === 'Rock' ||
+    playerSelection === 'Scissors' && computerSelection === 'Paper'
+  ) {
+    updateScore('player');
+    return `You Won! ${playerSelection} beats ${computerSelection}`
+  }
+
+  updateScore('pc');
+  return `You Lose! ${computerSelection} beats ${playerSelection}`
+}
+
+function playRound() {
   let playerChoice = this.dataset.key;
   let pcChoice = getComputerChoice();
 
-  let roundResult = playRound(playerChoice, pcChoice);
-
-  playerChoice = toCapital(playerChoice);
-  pcChoice = toCapital(pcChoice);
-
-  let message = `Round ${++round}: `;
-  if (roundResult === 'draw') {
-    message += `is a draw. Both choose ${playerChoice}`;
-  } else if (roundResult === 'player') {
-    message += `You Won! ${playerChoice} beats ${pcChoice}`;
-    playerScore++;
-  } else {
-    message += `You Lose! ${pcChoice} beats ${playerChoice}`;
-    pcScore++;
-  }
-  console.log(message);
+  appendRoundResult(`Round ${++round}: ${getResult(playerChoice, pcChoice)}`);
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', play));
-
-// let gameResult =
-//   playerScore === pcScore ? "There's no winner, result is a draw!" :
-//     playerScore > pcScore ? "Winner - player" : "Winner - pc";
-// return gameResult
-// console.log('result');
+const buttons = document.querySelectorAll('.choice button');
+buttons.forEach(button => button.addEventListener('click', playRound));
